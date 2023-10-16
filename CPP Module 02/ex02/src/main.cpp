00356b602ede60cb7dbbac6f26d2e94fa39b788c
Mathieu Roy <maroy@c3r11p7.42quebec.com>
2023-10-16 14:27:40 -0400

    presque fini cpp02

---
+++ CPP Module 02/ex02/src/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                     ██   ██ ██████         */
+/*   main.cpp                                          ██   ██      ██        */
+/*                                                     ███████  █████         */
+/*   By: maroy <maroy@student.42.qc>                        ██ ██             */
+/*                                                          ██ ███████.qc     */
+/*   Created: 2023/10/14 20:51:15 by maroy                                    */
+/*   Updated: 2023/10/16 14:27:05 by maroy            >(.)__ <(.)__ =(.)__    */
+/*                                                     (___/  (___/  (___/    */
+/* ************************************************************************** */
+
+#include "../inc/common.hpp"
+#include "../inc/Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	
+	try {
+    	Fixed result = a / 0; 
+    	cout << "Result: " << result << std::endl;
+	} catch (const std::invalid_argument& err) {
+    	cerr << "Error: " << err.what() << std::endl; // Handle the division by zero error
+	}
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
